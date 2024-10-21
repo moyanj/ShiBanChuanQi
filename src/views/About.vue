2bf12f9f3e03fb8b63d573e2282f9da04ebecd69
moyan <1561515308@qq.com>
2024-10-21 22:58:14 +0800

    feat: 更新版本号并添加关于页面内容
    - 将版本号从 0.0.0 修改为 1.0.0alpha
    - 在 About.vue 中添加版本信息、制作组名单和致谢内容
    - 引入新的 Element Plus 组件：ElScrollbar 和 ElCard

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-    import { ElRow, ElCol } from 'element-plus'
+    import { ElRow, ElCol, ElScrollbar, ElCard } from 'element-plus'
     import { useDataStore, DataStoreState } from '../js/store';
     import { getExplore } from '../js/utils';
     const data: DataStoreState = useDataStore();
-    
+
 
 </script>
 
@@ -13,14 +13,37 @@
             <div class="grid-content container">
                 <h1>十班传奇</h1>
                 <br>
+                <p>版本：{{ data.build_info.version }}</p>
                 <p v-if="data.is_electron">Electron版本：{{ data.build_info.electron_version }}</p>
-                <p v-if="data.is_electron">编译时间：{{ data.build_info.time }}</p>
+
+                <p>编译时间：{{ data.build_info.time }}</p>
                 <p> 浏览器版本：{{ getExplore() }}</p>
 
             </div>
         </el-col>
         <el-col :span="12">
-            <div class="grid-content container">About</div>
+            <div class="grid-content container">
+
+                <h2>制作组名单</h2>
+                <el-scrollbar style="height: 45vh; width: 100%;">
+                    <el-card>
+                        <p>莫颜：开发，策划，美术，UI</p>
+                        <p>辰哥：开发，策划</p>
+                        <p>太奶：策划</p>
+                        <p>镐京：策划</p>
+                        <p>范鸡：美术</p>
+                    </el-card>
+                </el-scrollbar>
+                <h2>致谢</h2>
+                <el-scrollbar style="height: 45vh; width: 100%;">
+                    <el-card>
+                        <p>
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;感谢由小米科技有限公司提供的MiSans字体。感谢由北京字节跳动科技有限公司，上海米哈游科技有限公司，Google Inc.，Leonid Tsvetkov提供的图标。感谢微软科技，VueJS团队，Github，北京阿里云计算科技提供的技术支持。<br>
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;最后，感谢重庆市凤鸣山中学全体师生，特别感谢重庆市风鸣山中学初2023级10班。
+                        </p>
+                    </el-card>
+                </el-scrollbar>
+            </div>
         </el-col>
     </el-row>
 
