a2735466df14ae48b74d392271852fe4df5d6629
moyan <1561515308@qq.com>
2024-11-02 21:27:18 +0800

    perf(audio): 优化按钮声音播放逻辑

--- src/components/sbutton.vue
+++ src/components/sbutton.vue
@@ -1,13 +1,13 @@
 <template>
-    <el-button @click="playSound" :type="type" :icon="icon" :disabled="disabled" :text="text" :size="size">
-      <slot></slot>
-    </el-button>
-  </template>
-  
-  <script setup>
+  <el-button @click="playSound" :type="type" :icon="icon" :disabled="disabled" :text="text" :size="size">
+    <slot></slot>
+  </el-button>
+</template>
+
+<script setup>
   import { ElButton } from 'element-plus';
   import { APM } from '../js/store';
-  
+
   const props = defineProps({
     type: {
       type: String,
@@ -25,18 +25,20 @@
       default: false,
     },
     size: {
-        type: String,
-        default: 'default',
+      type: String,
+      default: 'default',
     }
   });
-  APM.add("click_sound", 'audio/click.mp3')
-  const playSound = () => {
-    console.log("click");
+  if (!("click_sound" in APM.objs)) {
+    APM.add("click_sound", 'audio/click.mp3')
     APM.setVolume("click_sound", 0.3)
+  }
+
+  const playSound = () => {
     APM.play("click_sound");
   };
-  </script>
-  
-  <style scoped>
+</script>
+
+<style scoped>
   /* 你可以在这里添加自定义样式 */
-  </style>
+</style>
