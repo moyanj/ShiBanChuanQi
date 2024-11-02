92689659deec2b3a221f6dabe3d004a0d41c2577
moyan <1561515308@qq.com>
2024-11-03 00:55:40 +0800

    perf(Wish): 调整愿望列表爆率计算公式
    - 修改了愿望列表的爆率计算公式，将指数增长的速度放缓
    - 通过调整公式中的参数，降低了爆率的增长速度，提高了游戏的挑战性

--- src/views/Wish.vue
+++ src/views/Wish.vue
@@ -14,7 +14,7 @@
     wish_list.push(...Object.keys(characters))
 
     function f(x: number): number {
-        const value = 0.0001 + (Math.exp(x / 25) / 100);
+        const value = 0.0001 + (Math.exp(x / 35) / 175);
         const r = (value / 1); // 计算结果
 
         return r;
