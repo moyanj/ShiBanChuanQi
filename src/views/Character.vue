bc601709cfc934b3fabd8fbbb70ea90835f1ea7c
moyan <1561515308@qq.com>
2024-11-02 20:12:54 +0800

    feat(Character): 添加角色列表展示功能
    - 在 Character.vue 中添加角色列表展示功能
    - 使用 Element Plus 组件库实现角色卡片展示
    - 在角色卡片中添加角色图标和名称
    - 优化角色列表样式，增加底部间距

--- src/views/Character.vue
+++ src/views/Character.vue
@@ -1,13 +1,32 @@
 <script setup lang="ts">
-    import { ElCard, ElRow, ElCol, ElScrollbar } from 'element-plus';
-    
+    import { ElCard, ElRow, ElCol, ElScrollbar, ElImage } from 'element-plus';
+    import { CharacterType } from '../js/character';
+    import { useSaveStore } from '../js/store';
+    import { icons } from '../js/utils';
+
+    const save = useSaveStore();
+
+    const c2e = {
+        [CharacterType.Fire]: icons.element.fire,
+        [CharacterType.Grass]: icons.element.grass,
+        [CharacterType.LiangZi]: icons.element.liangzi,
+        [CharacterType.Nihility]: icons.element.nihility,
+        [CharacterType.Physics]: icons.element.physics,
+        [CharacterType.Thunder]: icons.element.thunder,
+        [CharacterType.Water]: icons.element.water,
+    }
+
+    const data = save.characters.characters;
 </script>
 
 <template>
     <el-row>
         <el-col :span="3">
             <el-scrollbar style="" class="menu">
-                <el-card class="item">1</el-card>
+                <el-card class="item" v-for="item in data">
+                    <el-image :src="c2e[item.type]" width="15px" height="15px"></el-image>
+                    <span class="name">{{item.name}}</span>
+                </el-card>
             </el-scrollbar>
         </el-col>
         <div class="verticalBar"></div>
@@ -23,6 +42,7 @@
         height: 100vh;
         width: 100%;
     }
+
     .verticalBar {
         width: 1px;
         height: 95vh;
@@ -31,9 +51,11 @@
         margin-left: 5px;
         margin-right: 5px;
     }
+
     .item {
         margin-right: 10px;
         background-color: #26272b;
+        margin-bottom: 10px;
     }
 
 </style>
