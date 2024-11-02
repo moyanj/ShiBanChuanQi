7ffbd1cb9759cc978a36df5dd6274d628dbcf2e6
moyan <1561515308@qq.com>
2024-11-02 23:26:05 +0800

    refactor(About): 重构关于页面布局并更新内容
    - 调整制作组名单和致谢名单的样式，优化滚动条设置
    - 更新致谢名单的显示方式，使用列表形式展示
    - 添加新的致谢对象到 zx_list 数组
    - 修正 Electron 版本显示逻辑
    - 优化页面样式，增加 zx 类样式

--- src/views/About.vue
+++ src/views/About.vue
@@ -16,6 +16,24 @@
         fluid: true
     }
     const show_cg = ref(false);
+
+    const zx_list = [
+        "小米科技有限公司",
+        "北京北大方正电子有限公司",
+        "北京字节跳动科技有限公司",
+        "上海米哈游科技有限公司",
+        "Google Inc.",
+        "微软科技",
+        "VueJS团队",
+        "Github",
+        "北京阿里云计算科技",
+        "Suno AI",
+        "Stable Diffusion团队",
+        "Electron团队",
+        "Flask团队",
+        "重庆市凤鸣山中学全体师生"
+    ]
+
     function play() {
         show_cg.value = true;
     }
@@ -29,7 +47,7 @@
                 <h1 id="title">十班传奇</h1>
                 <br>
                 <p v-if="data.build_info != null">版本：{{ data.build_info.version }}</p>
-                <p v-if="data.is_electron">Electron版本：{{ data.build_info.electron_version }}</p>
+                <p v-if="data.is_electron">Electron版本：{{ electron.version.electron }}</p>
                 <p v-if="data.is_electron">NodeJS版本：{{ electron.version.node }}</p>
                 <p v-if="data.build_info != null">编译时间：{{ data.build_info.time }}</p>
                 <p> 浏览器版本：{{ getExplore() }}</p>
@@ -41,24 +59,27 @@
             <div class="grid-content container">
 
                 <h2>制作组名单</h2>
-                <el-scrollbar style="height: 45vh; width: 100%;">
-                    <el-card>
-                        <p><span @click="play()">莫颜</span>：开发，策划，美术，UI</p>
-                        <p>辰哥：策划</p>
-                        <p>太奶：策划</p>
+                <el-card style=" width: 100%;">
+                    <el-scrollbar style="height: 25vh;">
+
+                        <p><span @click="play()">莫颜</span>：开发，策划，美术，编剧，测试</p>
+                        <p>辰哥：策划，测试</p>
+                        <p>太奶：策划，编剧</p>
                         <p>镐京：策划</p>
-                    </el-card>
-                </el-scrollbar>
+                    </el-scrollbar>
+                </el-card>
+
 
                 <h2>致谢名单</h2>
-                <el-scrollbar style="height: 45vh; width: 100%;">
-                    <el-card>
-                        <p>小米科技有限公司，北京北大方正电子有限公司，北京字节跳动科技有限公司，上海米哈游科技有限公司，Google Inc.，
-                            Leonid Tsvetkov，微软科技，VueJS团队，Github，北京阿里云计算科技，Suno AI， Stable
-                            Diffusion团队，Electron团队，Flask团队，重庆市凤鸣山中学全体师生。（排名不分先后）
-                        </p>
-                    </el-card>
-                </el-scrollbar>
+
+                <el-card class="zx">
+                    <el-scrollbar style="height: 25vh;">
+                        <li v-for="item in zx_list"><span>{{ item }}</span></li>
+                        <p>（排名不分先后）<br>衷心感谢每一位支持者，正是因为你们的帮助和鼓励，我们的项目才能不断向前推进。在这个过程中，您们的信任与支持是我们不断进步的动力。期待与大家共同见证更多的成就与精彩！</p>
+                        <p style="text-align: right;">2024年10月20日 莫颜</p>
+                    </el-scrollbar>
+                </el-card>
+
             </div>
         </el-col>
     </el-row>
@@ -90,5 +111,9 @@
         height: 36vh;
     }
 
-    
+    .zx {
+        margin-bottom: 15px;
+        width: 100%;
+    }
+
 </style>
