d22afeed9627316a64ceb0f7c4a9b3157271be92
moyan <1561515308@qq.com>
2024-11-08 21:19:16 +0800

    feat(setting): 初步实现设置界面

---
+++ src/components/setting-item.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+    const props = defineProps({
+        label: {
+            type: String,
+            required: true
+        }
+    })
+</script>
+
+<template>
+    <div class="item">
+        <p class="title">{{ props.label }}</p>
+        <slot></slot>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .item {
+        margin-bottom: 20px;
+        padding: 7px 15px 10px 15px;
+        width: 95vw;
+        border: 1px solid #58585B;
+        border-radius: 5px;
+        box-shadow: rgba(106, 98, 90, 0.1) 0px 8px 5px;
+    }
+
+    .title {
+        font-size: 18px;
+    }
+</style>
--- src/views/Setting.vue
+++ src/views/Setting.vue
@@ -1,12 +1,45 @@
 <script setup lang="ts">
+    import { ElScrollbar,ElMessageBox,ElMessage } from 'element-plus';
+    import { useSaveStore, useDataStore } from '../js/store';
+    import sbutton from '../components/sbutton.vue'
+    import settingItem from '../components/setting-item.vue'
+
+    function reset() {
+        ElMessageBox.confirm("确定删除吗？", {
+            type: "warning"
+        }).then(() => {
+            useSaveStore().$reset();
+            useDataStore().$reset();
+            ElMessage.success("删除成功")
+        })
+        
+    }
 </script>
 
 <template>
-    <div align="center">
-        <h1>设置</h1>
-        <p>开发中</p>
-    </div>
+    <h1 align="right">设置</h1>
+
+    <el-scrollbar class="content">
+        <settingItem label="云存档">
+            <sbutton type="primary">上传数据</sbutton>
+            <sbutton type="primary">加载数据</sbutton>
+            <p style="font-size:11px;">由Sqlpub和Render提供支持</p>
+        </settingItem>
+
+        <settingItem label="危险操作">
+            <sbutton type="danger" @click="reset">重置全部游戏数据</sbutton>
+        </settingItem>
+
+    </el-scrollbar>
+
 </template>
 
 <style scoped lang="scss">
+    .content {
+        height: 100vh;
+        width: 100vw;
+        padding-left: 10px;
+        padding-right: 10px;
+    }
+
 </style>
