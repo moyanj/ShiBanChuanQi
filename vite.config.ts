5f5ec84cbc670c93b69bd017cb90e2a937dd5970
moyan <1561515308@qq.com>
2024-11-10 15:45:12 +0800

    chore(dep): 更新依赖

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import {px2viewport} from '@mistjs/vite-plugin-px2viewport';
+import { px2viewport } from '@mistjs/vite-plugin-px2viewport';
 
 // 从package.json获取版本号
 const version = require('./package.json').version;
