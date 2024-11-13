17b397808c8972583ce5ce7240b8322a6ca6039d
moyan <1561515308@qq.com>
2024-11-13 22:01:24 +0800

    style(sbutton): 格式化代码

--- src/components/sbutton.vue
+++ src/components/sbutton.vue
@@ -1,45 +1,44 @@
 <template>
-  <el-button @click="playSound" :type="type" :icon="icon" :disabled="disabled" :text="text" :size="size">
-    <slot></slot>
-  </el-button>
+    <el-button @click="playSound" :type="type" :icon="icon" :disabled="disabled" :text="text" :size="size">
+        <slot></slot>
+    </el-button>
 </template>
 
 <script setup>
-  import { ElButton } from 'element-plus';
-  import { APM } from '../js/store';
+    import { ElButton } from 'element-plus';
+    import { APM } from '../js/store';
 
-  const props = defineProps({
-    type: {
-      type: String,
-    },
-    icon: {
-      type: String,
-      default: '',
-    },
-    disabled: {
-      type: Boolean,
-      default: false,
-    },
-    text: {
-      type: Boolean,
-      default: false,
-    },
-    size: {
-      type: String,
-      default: 'default',
-    },
-    "v-loading":{}
-  });
-  if (!("click_sound" in APM.objs)) {
-    APM.add("click_sound", 'audio/click.mp3')
-    APM.setVolume("click_sound", 0.3)
-  }
+    const props = defineProps({
+        type: {
+            type: String,
+        },
+        icon: {
+            type: String,
+            default: '',
+        },
+        disabled: {
+            type: Boolean,
+            default: false,
+        },
+        text: {
+            type: Boolean,
+            default: false,
+        },
+        size: {
+            type: String,
+            default: 'default',
+        },
+        "v-loading": {}
+    });
+    if (!("click_sound" in APM.objs)) {
+        APM.add("click_sound", 'audio/click.mp3')
+        APM.setVolume("click_sound", 0.3)
+    }
 
-  const playSound = () => {
-    APM.play("click_sound");
-  };
+    const playSound = () => {
+        APM.play("click_sound");
+    };
 </script>
 
 <style scoped>
-  /* 你可以在这里添加自定义样式 */
 </style>
