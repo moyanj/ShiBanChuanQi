a4f906e7ff63c67dcb986669abf6c945eeebc5ff
moyan <1561515308@qq.com>
2024-11-23 15:20:37 +0800

    feat(fight): 完善战斗界面人物展示

--- src/components/card.vue
+++ src/components/card.vue
@@ -1,9 +1,29 @@
 <script setup lang="ts">
+import { defineProps, computed } from 'vue';
+
+const props = defineProps({
+    character: {
+        type: String,
+        required: true
+    }
+})
+
+const backgroundStyle = computed(() => ({
+    backgroundImage: `url(illustrations/${props.character}.png)`
+}))
 </script>
 
 <template>
-
+  <div class="fight-card" :style="backgroundStyle">
+  </div>
 </template>
 
 <style lang="scss" scoped>
+.fight-card {
+    height: 240px;
+    width: 180px;
+    border: 3px solid green;
+    background-size: cover;
+    background-position: center;
+}
 </style>
